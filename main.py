fb5584b12ab484d0589f2aee0cb779ca1f753918
Rayhan Rani <rayhanrani25@gmail.com>
2020-08-17 00:59:56 -0700

    kjn
    ksbv

---
+++ main.py
@@ -0,0 +1,20 @@
+import random
+import time
+
+print("Welcome to Type Racer. Get ready to type.")
+time.sleep(1)
+print("Ready...")
+time.sleep(1)
+print("Set...")
+time.sleep(1)
+print("Type!")
+print()
+sentences = ["I went to the store to buy groceries", "I went outside to eat dinner","I play videogames throughout the summer vacation"]
+a = random.randint(0, 2)
+print(sentences[a])
+print()
+result = input("Type: ")
+start = time.time()
+end = time.time()
+print()
+print("You completed the typing in " + str(end - start) + " seconds. Good Job!")
