0f35aed3c2c572898eb00475da0552513878e34d
Rayhan Rani <rayhanrani25@gmail.com>
2020-08-17 10:12:11 -0700

    Description
    In this project, I was asked to use recursions to check if a word was a palindrome or not. A palindrome word is a word that is spelled out the same forwards and backward, so a great example would be "racecar". The recursion process used sublists to check the word from the right side and left side, and printed out either "True" or "False".

---
+++ Project 6: Recursions.py
@@ -0,0 +1,11 @@
+a = input("Type in a palindrome word: ")
+b=""
+def same (a):
+  if len(a) == 1:
+    print()
+    return True
+  elif a[0] != a[-1]:
+    print()
+    return False
+  return(same(a[1:-1]))
+print(same(a))
